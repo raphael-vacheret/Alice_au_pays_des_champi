b6fda17c87b71eecffb1753a33b478f7dd4fd332
Natacha Durac <natacha.durac@epfedu.fr>
2025-03-11 14:13:04 +0100

    changement kebab

--- src/js/niveau3.js
+++ src/js/niveau3.js
@@ -6,49 +6,65 @@ export default class niveau3 extends Phaser.Scene {
       key: "niveau3" //  ici on précise le nom de la classe en tant qu'identifiant
     });
   }
-  preload() {}
+  preload() {
+    this.load.image("tuile_1","src/assets/niveau3/kebab.png");
+    this.load.image("tuile_2", "src/assets/niveau3/legume.png");
+    this.load.image("tuile_3", "src/assets/niveau3/mcdo.png");
+    this.load.image("tuile_4", "src/assets/niveau3/piste.jpg");
+    this.load.tilemapTiledJSON("map", "src/assets/niveau3/map_burger.json");
+  }
 
   create() {
-    this.add.image(400, 300, "img_ciel");
     this.groupe_plateformes = this.physics.add.staticGroup();
-    this.groupe_plateformes.create(200, 584, "img_plateforme");
-    this.groupe_plateformes.create(600, 584, "img_plateforme");
+
+    const map = this.add.tilemap("map");
+    const tuile1 = map.addTilesetImage("kebab", "tuile_1");
+    const tuile2 = map.addTilesetImage("legume", "tuile_2");
+    const tuile3 = map.addTilesetImage("mcdo","tuile_3");
+    const tuile4 = map.addTilesetImage("piste", "tuile_4");
+    const calque2 = map.createLayer("fond", [tuile1,tuile2,tuile3,tuile4]);
+    const calque1 = map.createLayer("legumes", [tuile1,tuile2,tuile3,tuile4]);
+    
+    calque2.setCollisionByProperty({ estSolide: true });
+
     // ajout d'un texte distintcif  du niveau
     this.add.text(400, 100, "Vous êtes dans le niveau 3", {
       fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
       fontSize: "22pt"
     });
 
-    this.porte_retour = this.physics.add.staticSprite(100, 550, "img_porte3");
+    //this.porte_retour = this.physics.add.staticSprite(100, 550, "img_porte3");
 
     this.player = this.physics.add.sprite(100, 450, "img_perso");
     this.player.refreshBody();
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
+    this.physics.add.collider(this.player, calque2);
     this.clavier = this.input.keyboard.createCursorKeys();
     this.physics.add.collider(this.player, this.groupe_plateformes);
   }
 
   update() {
     if (this.clavier.left.isDown) {
-      this.player.setVelocityX(-160);
+      this.player.setVelocityX(-500);
       this.player.anims.play("anim_tourne_gauche", true);
     } else if (this.clavier.right.isDown) {
-      this.player.setVelocityX(160);
+      this.player.setVelocityX(500);
       this.player.anims.play("anim_tourne_droite", true);
     } else {
       this.player.setVelocityX(0);
       this.player.anims.play("anim_face");
     }
-    if (this.clavier.up.isDown && this.player.body.touching.down) {
+    if (this.clavier.up.isDown && this.player.body.blocked.down) // Vérifie qu'il ne touche pas calque2
+      {
       this.player.setVelocityY(-330);
     }
 
-    if (Phaser.Input.Keyboard.JustDown(this.clavier.space) == true) {
+    /*if (Phaser.Input.Keyboard.JustDown(this.clavier.space) == true) {
       if (this.physics.overlap(this.player, this.porte_retour)) {
         console.log("niveau 3 : retour vers selection");
         this.scene.switch("selection");
       }
-    }
+    }*/
   }
 }
