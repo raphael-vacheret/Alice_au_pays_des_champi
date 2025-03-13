a855d69f3a5bd9794ab9683b5771066bd98d5566
Natacha Durac <natacha.durac@epfedu.fr>
2025-03-13 18:08:44 +0100

    fin fin

--- src/js/niveau4_fin.js
+++ src/js/niveau4_fin.js
@@ -5,7 +5,7 @@ export default class niveau4_fin extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('fin_casino', 'src/assets/niveau4/fin_casino.jpg');
+        this.load.image('fin_casino', 'src/assets/niveau4/fin_casino.png');
         this.load.audio('son_casino', 'src/assets/niveau4/son_casino.mp3');
     }
 
@@ -19,12 +19,6 @@ export default class niveau4_fin extends Phaser.Scene {
         
         let image0 = this.add.image(this.scale.width / 2, this.scale.height / 2, "fin_casino");
         image0.setDisplaySize(this.scale.width, this.scale.height);
-        zone_texte = this.add.text(200, 400, "OUPSI TA PLUS D ARGENT TU AURAI DU ARRETER DE PARIER", {
-                fontSize: "bold 32px Arial",
-                fill: "#2F5BFD"
-        });
-
-        
 
         this.time.addEvent({
             delay: 7500, // nouvelle valeur pour random et direction
