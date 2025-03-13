693bbb531463c1207f4fbc4d6e727ea9a8cb0d34
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2025-03-13 20:03:37 +0100

    aaaaa

--- src/js/niveau_2/niveau2_1_2.js
+++ src/js/niveau_2/niveau2_1_2.js
@@ -82,7 +82,7 @@ export default class niveau2_1_2 extends Phaser.Scene {
       if (Phaser.Input.Keyboard.JustDown(this.clavier.space) == true) {
         if (this.physics.overlap(this.player, this.porte_retour)) {
           console.log("vrai");
-          this.music.stop();
+          this.sound.stopAll();
           this.scene.switch("niveau2_fin");
         }
         if (this.physics.overlap(this.player, this.porte_3)) {
